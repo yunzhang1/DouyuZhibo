b01f5ea909281d2f0b253425c0c43f2948384962
leadhang <leadhang@leadhangdemac-mini.local>
2017-02-08 10:39:49 +0800

    初始化项目

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by leadhang on 2017/2/8.
+//  Copyright © 2017年 leadhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
